ebfd465f99814a3dd23c45fa38b8b1c572c38574
Sathiya Naraayanan <77498786+born2win685@users.noreply.github.com>
2021-12-30 15:18:38 +0530

    Update Homescreen.cpp

--- Homescreen.cpp
+++ Homescreen.cpp
@@ -1,12 +1,12 @@
 #include "Homescreen.h"
 
-void Window::makeWindow(const char* title, int width, int height)
+void Window::makeWindow(const char* title, int width, int height,const char* path)
 {
     if(SDL_Init(SDL_INIT_EVERYTHING)!=0)
     {
         cout<<"error: Failed to initialize "<< SDL_GetError()<<endl;    
     }
-    window = SDL_CreateWindow("Ammomatics v1.0",0 , 0 ,width,height,0);
+    window = SDL_CreateWindow(title,SDL_WINDOWPOS_CENTERED,0 ,width,height,0);
     
     if(window==NULL)
             cout<<"Couldn't create window.   error"<< SDL_GetError() << endl;
@@ -22,7 +22,7 @@ void Window::makeWindow(const char* title, int width, int height)
     SDL_Rect dest;
     dest.x=0;
     dest.y=100;
-    bg_image=SDL_LoadBMP("images/back.bmp");
+    bg_image=SDL_LoadBMP(path);
     SDL_BlitSurface(bg_image, NULL,surface,&dest);
     SDL_UpdateWindowSurface(window);
 }
@@ -51,7 +51,13 @@ void Window::set_button(const char* path,int p_x,int p_y)
 
 }
 
-void Buttons::mouse_click(int p_x,int p_y,int b_w,int b_h,int flag)
+void Window::DestroyWindow()
+{
+    SDL_DestroyWindow(window);
+}
+
+
+void Buttons::mouse_click()
 {
     SDL_Event mouse_event;
     bool quit =true;
@@ -65,18 +71,57 @@ void Buttons::mouse_click(int p_x,int p_y,int b_w,int b_h,int flag)
             quit =false; 
             break;
         case SDL_MOUSEBUTTONDOWN: 
+        {
             mx=mouse_event.motion.x;
             my=mouse_event.motion.y;
-            if(mx>p_x && mx<(p_x+b_w) && my<(p_y+b_h) && my>p_y && mouse_event.type== SDL_MOUSEBUTTONDOWN)
+            if(mx>100 && mx<301 && my<635 && my>520)  
                 cout<<"clicked"<<endl;
+            if(mx>1100 && mx<1205 && my<635 && my>520)  
+            {
+                cout<<"instructions"<<endl;
+                Window* k= new Window();
+                k->makeWindow("Instructions-click anywhere to close",715,722,"images/i.bmp");
+                SDL_Event event;
+                bool quit=true;
+                while(quit)
+                {
+                    while(SDL_PollEvent(&event)!=0)
+                    {
+                        if(event.type == SDL_MOUSEBUTTONDOWN)
+                            quit=false;
+                    }
+                }
+                cout<<"done"<<endl;
+                k->DestroyWindow();
+            }
+            if(mx>1100 && mx<1200 && my<100 && my>0)
+            {
+              Window* q= new Window();
+              q->makeWindow("High Score-click anywhere to close",715,722,"images/i.bmp");
+              SDL_Event event;
+                bool quit=true;
+                while(quit)
+                {
+                    while(SDL_PollEvent(&event)!=0)
+                    {
+                        if(event.type == SDL_MOUSEBUTTONDOWN)
+                            quit=false;
+                    }
+                }
+                cout<<"done"<<endl;
+                q->DestroyWindow();
+            }    
             break;
-        default:
-            break;
-    }    
-    }  
-        
+            default:
+                break;
+        }                
+                       
     }
-}
+                   
+    }
+    }
+}    
+    
 
 
 Buttons::Buttons()
@@ -87,4 +132,68 @@ Buttons::Buttons()
 Buttons::~Buttons()
 {
 
+}
+
+void HighscoreManager::get_username(SDL_Window* window,SDL_Renderer* ren)
+{
+        SDL_Color colour={255,0,0,255};
+        Text t(ren,"font.ttf",30,"Enter name",colour);
+        t.display(100,100,ren);
+        SDL_UpdateWindowSurface(window);
+        bool quit=true;
+        SDL_Event e;
+        SDL_StartTextInput();
+        while(quit)
+        {
+            while(SDL_PollEvent(&e))
+            {
+                if(e.type==SDL_QUIT)
+                    quit= false;
+                else if(e.type==SDL_TEXTINPUT || e.type==SDL_KEYDOWN)
+                {
+                    system("clear");
+                    if(e.type==SDL_KEYDOWN && e.key.keysym.sym==SDLK_BACKSPACE && username.length()>0)
+                        username.pop_back();
+                    else if(e.type==SDL_TEXTINPUT)
+                        username+=e.text.text;
+                }                
+            }
+        }
+        SDL_StopTextInput();
+}
+
+void HighscoreManager::add_score()
+{
+    fstream f("score.txt",ios::out | ios::app);
+    if(f.is_open())
+    {
+        for(int i=0; i<username.size(); i++)
+            f.put(username.at(i));
+        f.put('\n');
+        f.close();
+    }
+
+
+}
+
+SDL_Texture *Text::loadfont(SDL_Renderer *renderer, const string &f_path,int f_size,const string &message_text,const SDL_Color &color)
+{
+    TTF_Font *font = TTF_OpenFont(f_path.c_str(),f_size);
+    auto text_surface=TTF_RenderText_Solid(font,message_text.c_str(),color);
+    auto text_texture=SDL_CreateTextureFromSurface(renderer,text_surface);
+    SDL_FreeSurface(text_surface);
+    return text_texture;
+}
+
+void Text::display(int x,int y,SDL_Renderer *renderer)const
+{
+    Rect.x=x;
+    Rect.y=y;
+    SDL_RenderCopy(renderer,texture,nullptr,&Rect);
+}
+
+Text::Text(SDL_Renderer *renderer,const string &f_path,int f_size,const string &message,const SDL_Color &color)
+{
+    texture= loadfont(renderer,f_path,f_size,message,color);
+    SDL_QueryTexture(texture,nullptr,nullptr,&Rect.w,&Rect.h);
 }
