ba5f5fbcf8bb9e1b1f969e9d40191d1d9f415b1e
Sathiya Naraayanan <77498786+born2win685@users.noreply.github.com>
2021-12-31 20:49:04 +0530

    Add files via upload

--- win.cpp
+++ win.cpp
@@ -16,6 +16,6 @@ int main(int argc, char* args[])
     win->makeWindow("Game over",1350,650,"images/back.bmp");
     h->get_username(win->window,win->renderer);
     win->DestroyWindow();
-    h->add_score(2300);    
+    h->add_score(46906);   
     return 0;
 }
